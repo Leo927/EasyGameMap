11d214f3a83852bdb7acfdf27b2fe3b1ed30dc22
Songhao <songhao.li@hotmail.com>
2022-03-25 14:21:05 -0600

    Added navbar

--- components/nav-bar/nav-bar.js
+++ components/nav-bar/nav-bar.js
@@ -1,19 +1,22 @@
 import * as React from 'react';
-import { View, Pressable, StyleSheet, Text, Button, TouchableOpacity } from 'react-native';
+import { View, Pressable, StyleSheet, Text, Button, TouchableOpacity, Modal } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import EGMContext from '../../context';
+import UserModal from '../usermodal/usermodal';
 
 export default function NavBar(){
+    const context = React.useContext(EGMContext);
+
+    const navigation = useNavigation();
+
+    const [ modalVisible, setModalVisible ] = React.useState(false);
+    
     return(
-        <View style={styles.navbar}>            
-            <Text>Nav Name</Text>
-            
-            <TouchableOpacity
-                style={styles.navElement}>
-                <Text>A</Text>
-            </TouchableOpacity>
-            
-            <TouchableOpacity
-                style={styles.navElement}>
-                <Text>B</Text>
+        <View style={styles.navbar}>    
+            <UserModal modalVisible={modalVisible} setModalVisible={setModalVisible}/>
+            <TouchableOpacity 
+                onPress={()=>setModalVisible(true)}>
+                <Text>{context.uname}</Text>
             </TouchableOpacity>
         </View>
     );
@@ -27,8 +30,7 @@ const styles = StyleSheet.create({
         alignContent: 'flex-end',
     },
     navElement:{
-        width: 30,
-        backgroundColor: 'red',
-        margin: 2
+        margin: 5,
+        justifyContent: 'center'
     },
 });
