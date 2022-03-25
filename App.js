127b9b7ab142b8050cdd9ead6a62b54a6297b384
Songhao <songhao.li@hotmail.com>
2022-03-25 14:21:29 -0600

    added navigation screens

--- App.js
+++ App.js
@@ -3,27 +3,57 @@ import { Button, View, Text, TextInput } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import EGMContext from './context';
 import NavBar from './components/nav-bar/nav-bar';
+import UserMaps from './components/usermaps/usermaps';
+import MapConfig from './components/map-config/map-config';
 
 const Stack = createNativeStackNavigator();
 
-const App = () => {
+const App = () => {  
+  const [uname, setUname] = React.useState("Dummy user");
+  const [uid, setUid] = React.useState("Dummy uid");
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <EGMContext.Provider
+      value={{uname, setUname, uid, setUid}}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='MapConfig'>
+          
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              headerRight: ()=>(<NavBar/>)
+            }}
+          />
+
+          <Stack.Screen
+            name="UserMaps"
+            component={UserMaps}
+            options={{
+              headerRight: ()=>(<NavBar/>)
+            }}
+          />
+
+          <Stack.Screen
+            name="MapConfig"
+            component={MapConfig}
+            options={{
+              headerRight: ()=>(<NavBar/>)
+            }}
+            initialParams = {{mapId:"TestMapId"}}
+          />
+
+
+        </Stack.Navigator>
+      </NavigationContainer>
+    </EGMContext.Provider>
   );
 };
 
 const HomeScreen = ({ navigation }) => {
   return (
       <View>
-        <NavBar></NavBar>
         <Text>
           Test
         </Text>
