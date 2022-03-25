2498787ab24b6f09e997bd0831cd9d6a71397fff
Songhao <songhao.li@hotmail.com>
2022-03-25 14:26:56 -0600

    added map class

---
+++ classes/map.js
@@ -0,0 +1,14 @@
+class Map{
+    constructor(){
+        this.name = "";
+        this.id = "";
+        this.img = "";
+        this.width = 0;
+        this.height = 0;
+        this.markerGroups = {};
+        this.icons = {};
+        this.markers = {};
+    }
+}
+
+export {Map};
