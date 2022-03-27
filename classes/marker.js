433e7f90b75db900c9dde0ba3eb7f55580a96fbd
Songhao <songhao.li@hotmail.com>
2022-03-27 01:30:59 -0600

    fixed marker class not setting class variables

--- classes/marker.js
+++ classes/marker.js
@@ -4,35 +4,35 @@
 class Marker{
     constructor(){
         //unique id of the marker,
-        _id = "";
+        this._id = "";
 
         // Title of marker. Shown in the popup modal as title.
-        title = "";
+        this.title = "";
 
         // Detailed description of the marker.
-        description = "";
+        this.description = "";
 
         // shown underneath the icon.
-        label = "";
+        this.label = "";
         
         // If true, the icon used will be from the customIcons. 
         // Other wise, the icon id points to a default icon. 
-        isCustomIcon = false;
+        this.isCustomIcon = false;
         
         // id of the icon. See isCustomIcon for more detail 
         // of where it comes from. 
-        iconId = "";
+        this.iconId = "";
 
         // number representing the distance of 
         // the center of icon from the left edge of the map. 
-        left = 0;
+        this.left = 0;
 
         // number representing the distance of the 
         // center of the icon from the top of the map. 
-        top = 0;
+        this.top = 0;
 
         // A string pointing to one of the markerGroups from the map. 
-        markerGroup = "";
+        this.markerGroup = "";
 
     }
 }
