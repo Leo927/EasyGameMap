d3674c262db6aad59d3b28b2156b81b3c6e8fa11
Songhao <songhao.li@hotmail.com>
2022-03-27 20:50:55 -0600

    adjusted indentation

--- classes/map.js
+++ classes/map.js
@@ -1,14 +1,14 @@
 class Map{
-    constructor(){
-        this._id = "";
-        this.name = "";
-        this.image = "";
-        this.width = "";
-        this.height = "";
-        this.markerGroups = [];
-        this.customIcons = {};
-        this.markers = {};
-    }
+  constructor(){
+      this._id = "";
+      this.name = "";
+      this.image = "";
+      this.width = "";
+      this.height = "";
+      this.markerGroups = [];
+      this.customIcons = {};
+      this.markers = {};
+  }
 }
 
 export {Map};
