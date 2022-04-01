341b598f66e8c6e1d2043e907c1b1260c47ef884
Songhao <songhao.li@hotmail.com>
2022-03-31 18:54:15 -0600

    Implemented home screen

--- components/home-screen/home-screen.js
+++ components/home-screen/home-screen.js
@@ -1,18 +1,31 @@
-import {View, StyleSheet} from 'react-native';
+import React from 'react';
+import { View, StyleSheet, FlatList } from 'react-native';
 import { Text } from 'react-native-paper';
-import EgmMapView from '../map-view/map-view';
+import MapCard from '../map-card/map-card';
+import { getMaps } from '../../data/map';
 
-export default function HomeScreen({navigation}) {
-    return (
-        <View style={styles.container}>
-            <Text>This is home screen</Text>
-            <EgmMapView></EgmMapView>
-        </View>
-    );
+
+export default function HomeScreen() {
+  const [maps, setMaps] = React.useState([]);
+
+  // on reload grab all maps
+  React.useEffect(async () => {
+    setMaps(await getMaps());
+  }, [])
+
+  return (
+    <View style={styles.container}>
+      <FlatList 
+        data={maps}
+        keyExtractor={(m)=>m._id}
+        renderItem={({item, index})=>(<MapCard map={item}/>)}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-    }
+  container: {
+    flex: 1,
+  }
 })
