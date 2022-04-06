225470d21acc292fe0443d75f4a0f4fd2b1af61f
Songhao <songhao.li@hotmail.com>
2022-04-05 19:55:04 -0600

    Used MapCard for UserMaps

--- components/map-card/map-card.js
+++ components/map-card/map-card.js
@@ -1,38 +1,37 @@
 import { Card, Button } from "react-native-paper";
-import { StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
-function MapCard({ map, canEdit }) {
+/**
+ * Implements a map card to show a map in a list
+ * Props: 
+ * <1> map: the map data to display
+ * <2> canEdit: Boolean flag on whether the map card can be edited. 
+ * <3> onDelete: Handler to be called when Delete is pressed. The map is passed in as argument.
+ */
+function MapCard({ map, canEdit, onDelete }) {
   const navigation = useNavigation();
   return (
-    <Card style={styles.mapCard}>
-      <Card.Title title={map?.name?map.name: "INVALID"} />
+    <Card>
+      <Card.Title title={map?.name ? map.name : "INVALID"} />
       <Card.Actions>
-        <Button onPress={()=>navigation.navigate("MapViewScreen", {mapId: map._id})}>Open</Button>
+        {/* Open the map in view mode. */}
+        <Button onPress={() => navigation.navigate("MapViewScreen", { mapId: map._id })}>Open</Button>
 
+        {/* Open the edit page on the map. */}
         {canEdit && map?._id != undefined &&
           <Button onPress={() => navigation.navigate("MapConfigStack", { mapId: map._id })}>Edit</Button>}
 
-        {canEdit && map?._id != undefined &&
-          <Button onPress={() => {
-            setDelVisible(true);
-            setDelTarget(map._id);
-          }}>Delete</Button>
-        }
+        {/* Delete the Map */}
+        {canEdit && map?._id != undefined && onDelete != undefined &&
+          <Button onPress={() => onDelete(map)}>Delete</Button>}
 
       </Card.Actions>
     </Card>
   );
 }
 
-const styles = StyleSheet.create({
-  mapCard:{
-    
-  }
-})
-
-MapCard.defaultProps ={
-  canEdit : false
+MapCard.defaultProps = {
+  canEdit: false
 }
 
 export default MapCard;
--- components/usermaps/usermaps.js
+++ components/usermaps/usermaps.js
@@ -4,16 +4,24 @@ import { useNavigation, useRoute, useFocusEffect } from '@react-navigation/nativ
 import EGMContext from '../../context';
 import { getUserMaps, deleteMap, updateMap } from '../../data/map';
 import { Card, Button, Text, Title, Portal, Dialog, Provider, Paragraph } from 'react-native-paper';
+import MapCard from '../map-card/map-card';
 
+/**
+ * Implements a page to display user maps. 
+ * 
+ * @param {*} param0 
+ * @returns 
+ */
 export default function UserMaps({uid}){
     const navigation = useNavigation();
 
+    // control whether the delete dialog is visible
     const [delVisible, setDelVisible] = React.useState(false);
 
-    const [delAction, setDelAction] = React.useState(()=>{});
-
+    // store the delete target. Should be a mapId
     const [delTarget, setDelTarget] = React.useState(null);
 
+    // global context
     const context = React.useContext(EGMContext);
 
     // displayed maps. changed whenever params.uid changes.
@@ -38,23 +46,36 @@ export default function UserMaps({uid}){
       updateMaps();
     },[]));
 
-    const renderMap = ({item, index})=>{
-        return(
-        <Card style={styles.mapCard}>
-            <Card.Title title={item.name}/>
-            <Card.Actions>
-                <Button onPress={()=>navigation.navigate("MapConfigStack", {mapId: item._id})}>Config</Button>
-                <Button onPress={()=>navigation.navigate("MapViewScreen", {mapId: item._id})}>Open</Button>
-                <Button onPress={()=>{
-                    setDelVisible(true);
-                    setDelTarget(item._id);
-                }}>Delete</Button>
-            </Card.Actions>
-        </Card>);
-    };
+    // handle pressing delete button on the map card
+    function onDeletePressed(map){
+      // set map as delete target
+      setDelTarget(map._id);
+      // set delete dialog visible
+      setDelVisible(true);
+    }
+    
+    // handle pressing confirm delete
+    function onConfirmDeleteMap(){
+      if(!delTarget)
+        return;
+      deleteMap(delTarget, context.user)
+      .then((res)=>{
+          if(res.deletedCount){
+              setMaps(maps.filter(m=>m._id != delTarget))                                    
+          }
+          setDelVisible(false);                                
+      })
+      .catch((e)=>{
+          console.error(e);
+      })
+  
+    }
+
 
     return(
         <View style={styles.container}>
+
+            {/* A delete confirmation dialog */}
             <Portal>
                 <Dialog visible = {delVisible} onDismiss={()=>setDelVisible(false)}>
                     <Dialog.Title>Delete Map</Dialog.Title>
@@ -62,22 +83,12 @@ export default function UserMaps({uid}){
                         <Paragraph>Are you sure you want to delete the map?</Paragraph>
                     </Dialog.Content>
                     <Dialog.Actions>
-                        <Button onPress={()=>{
-                            deleteMap(delTarget, context.user)
-                            .then((res)=>{
-                                if(res.deletedCount){
-                                    setMaps(maps.filter(m=>m._id != delTarget))                                    
-                                }
-                                setDelVisible(false);                                
-                            })
-                            .catch((e)=>{
-                                console.error(e);
-                            })
-                        }}>Confirm</Button>
+                        <Button onPress={onConfirmDeleteMap}>Confirm</Button>
                         <Button onPress={()=>setDelVisible(false)}>Cancel</Button>
                     </Dialog.Actions>
                 </Dialog>
             </Portal>
+
             {/* Only render create new map button when loggined and user id match map list id */}
             {context.user?.uid && 
                 (context.user.uid== uid) &&
@@ -86,7 +97,9 @@ export default function UserMaps({uid}){
                 <FlatList
                     data={maps}
                     keyExtractor ={item=>item._id.toString()}
-                    renderItem = {renderMap}
+                    renderItem = {({item, index})=>(
+                      <MapCard canEdit={item.uid==context.user.uid} map={item} 
+                        onDelete={()=>onDeletePressed(item)}/>)}
                 />
             </View>
         </View>
