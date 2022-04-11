d7753abe1461fe949cc5ae19a5e0c9df8e45e716
Leo927 <asd347925290@gmail.com>
2022-04-11 12:17:08 -0600

    Added docs for nav-bar. Refactored fetch user

--- components/nav-bar/nav-bar.js
+++ components/nav-bar/nav-bar.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet, TouchableOpacity, View, Alert} from 'react-native';
+import { StyleSheet, TouchableOpacity, View, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import EGMContext from '../../context';
 import { Avatar, Portal, Dialog, Button } from 'react-native-paper';
@@ -7,8 +7,10 @@ import { CLIENT_ID, SERVER_URL } from '../../secrets';
 import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
 import * as WebBrowser from 'expo-web-browser';
 
+// Close the authentication page on web
 WebBrowser.maybeCompleteAuthSession();
 
+// discovery object used for authentication
 const discovery = {
   authorizationEndpoint: 'https://github.com/login/oauth/authorize',
   tokenEndpoint: 'https://github.com/login/oauth/access_token',
@@ -18,105 +20,117 @@ const discovery = {
 /**
  * Handles login and logout
  */
-export default function NavBar(){
-    const context = React.useContext(EGMContext);
+export default function NavBar() {
+  const context = React.useContext(EGMContext);
 
-    const navigation = useNavigation();
+  // controls whether the dialog is visible
+  const [visible, setVisible] = React.useState(false);
 
-    const [visible, setVisible] = React.useState(false);
-  
-    const [request, response, promptAsync] = useAuthRequest(
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: CLIENT_ID,
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({ useProxy: true })
+    },
+    discovery
+  );
+
+  // Listen to login response. 
+  // Takes the returned code and ask server for access token. 
+  React.useEffect(async () => {
+    if (response?.type === 'success') {
+      try {
+        const { code } = response.params;
+        const exchangeTokenEndPoint = SERVER_URL + `/oauth2/exchange/github/${code}`;
+        const res = await fetch(exchangeTokenEndPoint);
+        if (!res.ok)
+          throw "Server Error";
+        const resJson = await res.json();
+        context.setUser(u => ({ ...u, token: resJson.token }));
+      } catch (e) {
+        console.error(`Failed to exchange token`, e);
+        return;
+      }
+    }
+  }, [response]);
+
+  /**
+   * Handles user clicking login
+   */
+  const onLogin = () => {
+    promptAsync({ useProxy: true })
+    setVisible(false);
+  }
+
+  /**
+   * Fetch the user given token.
+   * @param {string} token
+   * @returns 
+   * The user {uid, uname, token}
+   */
+  async function fetchUser(token){
+    const userEndPoint = SERVER_URL + `/oauth2/user/github`;
+    const res = await fetch(userEndPoint,
       {
-        clientId: CLIENT_ID,
-        scopes: ['identity'],
-        redirectUri: makeRedirectUri({useProxy:true})
-      },
-      discovery
-    );
-
-    // Listen to login response. 
-    // Takes the returned code and ask server for access token. 
-    React.useEffect(async () => {
-      if (response?.type === 'success') {
-        try{
-          const { code } = response.params;
-          const exchangeTokenEndPoint = SERVER_URL + `/oauth2/exchange/github/${code}`;
-          const res =  await fetch(exchangeTokenEndPoint);
-          if(!res.ok)
-            throw "Server Error";
-          const resJson = await res.json();
-          context.setUser(u=>({...u,  token:resJson.token}));
-        }catch(e){
-          console.error(`Failed to exchange token`, e);
-          return;
-        }
-      }
-    }, [response]);
-
-    /**
-     * Handles user clicking login
-     */
-    const onLogin = ()=>{
-      promptAsync({useProxy:true})
-      setVisible(false);
+        method: 'POST',
+        body: JSON.stringify({ token }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+    if (!res.ok) {
+      throw Error("Failed to login. Bad Responses.");
     }
-
-    /**
-     * Watch user token change and fetch username and uid from token. 
-     */
-    React.useEffect(async()=>{
-      if(!context.user?.token)
-        return;
-      const token = context.user.token;
-      const userEndPoint = SERVER_URL + `/oauth2/user/github`;
-      const res =  await fetch(userEndPoint,
-        {
-          method: 'POST',
-          body:JSON.stringify({token}),
-          headers: {'Content-Type': 'application/json'}
-        });
-      if(!res.ok){
-        Alert.alert("Failed to login. Bad Response.");
-        return;
-      }
-      const resJson = await res.json();
-      if(!resJson.uid||!resJson.uname || !resJson.token){
-        Alert.alert("Failed to login");
-        return;
-      }
-      context.setUser(resJson);
-      
-      console.log(`user changed`, context.user);
-    },[context.user?.token])
-
-    function logout(){
-      context.setUser(undefined);
+    const resJson = await res.json();
+    if (!resJson.uid || !resJson.uname || !resJson.token) {
+      throw Error("Failed to login. Response body invalid.");
     }
+    return {uid: resJson.uid, uname: resJson.uname, token: resJson.token};
+  }
 
-    return(
-        <View>
-            <Portal>
-                <Dialog visible={visible} onDismiss={()=>setVisible(false)}>
-                    <Dialog.Content>
-                        {!context.user && 
-                        <Button mode='outlined' onPress={onLogin}>Login</Button>}
+  /**
+   * Watch user token change and fetch username and uid from token. 
+   */
+  React.useEffect(async () => {
+    if (!context.user?.token)
+      return;
+    var currentUser;
+    try{
+      currentUser = fetchUser(context.user.token);
+    }catch(e){
+      Alert.alert("Failed to fetch user. " + e);
+      return;
+    }
+    context.setUser(currentUser);
+  }, [context.user?.token])
 
-                        <Button mode='outlined'>Options</Button>
-                        
-                        {context.user&&
-                        <Button mode='outlined' onPress={logout}>Logout</Button>}
-                    </Dialog.Content>
-                </Dialog>
-            </Portal>
-            <TouchableOpacity onPress={()=>setVisible(true)}>
-            <Avatar.Text 
-                size={34} 
-                label={context.user?.uname?context.user.uname[0]: "G"} 
-                />
-            </TouchableOpacity>
-        </View>
-    );
+  function logout() {
+    context.setUser(undefined);
+  }
+
+  return (
+    <View>
+      <Portal>
+        <Dialog visible={visible} onDismiss={() => setVisible(false)}>
+          <Dialog.Content>
+            {!context.user &&
+              <Button mode='outlined' onPress={onLogin}>Login</Button>}
+
+            {/* Placeholder */}
+            {/* TODO - navigate to options page */}
+            <Button mode='outlined'>Options</Button>
+
+            {context.user &&
+              <Button mode='outlined' onPress={logout}>Logout</Button>}
+          </Dialog.Content>
+        </Dialog>
+      </Portal>
+
+      {/* Button to show the dialog */}
+      <TouchableOpacity onPress={() => setVisible(true)}>
+        <Avatar.Text
+          size={34}
+          label={context.user?.uname ? context.user.uname[0] : "G"}
+        />
+      </TouchableOpacity>
+    </View>
+  );
 }
-
-const styles = StyleSheet.create({
-});
